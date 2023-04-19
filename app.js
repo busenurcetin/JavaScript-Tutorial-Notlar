f40335cc372d5c92dd2d9912861071483075b231
Buse Nur Çetin <busenurcet@gmail.com>
2023-04-19 22:49:53 +0300

    Yeni Notlar Eklendi.

--- app.js
+++ app.js
@@ -1300,9 +1300,201 @@ Math Sınıfının İÇinde Floor Diye Bir Sınıf Var Bu Sınıf Bizim Sayımı
      function FiltreliUrunleriYazdir(urunler){
         urunler.forEach(function(urun){
             console.log("--------------------------");
-            console.log("|"+urub.isim+"|"+urun.fiyat+"|"+urun.kategori);
+            console.log("|"+urun.isim+"|"+urun.fiyat+"|"+urun.kategori);
             console.log("--------------------------");
         });
      }
+
+        * Diziler Örnek *
+
+     * Ürün Bulma Uygulaması *
+
+     let urun1 = {
+        isim: "Macbook",
+        kategori: "Teknoloji",
+        fiyat: 83.239;
+     }
+     let urun2 = {
+        isim: "Lenova",
+        kategori: "Teknoloji",
+        fiyat: 43.239;
+     }
+     let urun3 = {
+        isim: "Ipad",
+        kategori: "Teknoloji",
+        fiyat: 23.239;
+     }
+     let urun4 = {
+        isim: "Huwaei",
+        kategori: "Teknoloji",
+        fiyat: 53.239;
+     }
+     let urun5 = {
+        isim: "Casper",
+        kategori: "Teknoloji",
+        fiyat: 13.239;
+     }
+     let urun6 = {
+        isim: "Excalibur",
+        kategori: "Teknoloji",
+        fiyat: 82.339;
+     }
+
+     Gerçek hayatta böyle kullanılmaz. Veri tabanından çekilir.
+
+     let urunler = [urun1,urun2,urun3,urun4,urun5,urun6];
+     let filtreli =[];
+     let kullanıcıIsım= propmt("Bir Ürün İsmi Giriniz.");
+
+     FiltreliUrunleriDoldur(urunler);
+     FiltreliUrunleriYazdir(filtreli);
    
+     function FiltreliUrunleriDoldur(urunler){
+        urunler.forEach(function(urun){
+        if(urun.isim.toUpperCase().include(kullanıcıIsım.toUpperCase,0)){
+            filtreli.push(urun);
+        }
+     });
+     }
+
+     function write FiltreliUrunleriYazdir(urunler){
+        urunler.forEach(function(urun){
+            console.log("-----------------");
+            cosole.log("|"+ urun.isim+"|"+urun.fiyat+"|"+urun.kategori);
+            console.log("-----------------");
+        });
+     }
+
+         * Örnek 2 - Kitap Bulma Uygulaması *
+
+     Raflar oluşturacağız ve yazdığımız kitapların kaçıncı rafta olduğunu kullanıcı sorunca söyleyeceğiz.
+
+     let kitap1 ={ 
+        isim= "Her Şeyi Düşünme", 
+        yazar: "Anne Bogel",
+        fiyat: 25
+        raf: "1.5.RAF"
+     }
+     let kitap2 ={ 
+        isim= "Hiçbir Karşılaşma Tesadüf Değildir", 
+        yazar: "Hakan Mengüç",
+        fiyat: 32
+        raf: "2.3.RAF"
+     }
+     let kitap3 ={ 
+        isim= "İnsan Neyle Yaşar", 
+        yazar: "Tolstoy",
+        fiyat: 34
+        raf: "3.3.RAF"
+     }
+     let kitap4 ={ 
+        isim= "Zafer Sızlanarak Kazanılmaz", 
+        yazar: "Haluk Tatar",
+        fiyat: 45
+        raf: "4.5.RAF"
+     }
+     let kitap5 ={ 
+        isim= "Şeker Portakalı", 
+        yazar: "Jose",
+        fiyat: 50
+        raf: "1.2.RAF"
+     }
+
+
+     let kitaplar [kitap1,kitap2,kitap3,kitap4,kitap5];
+
+     5 raftan oluşan 25 kitap tutabilen bir sistem gibi düşünün.
+
+     let raf11={kod: "1.1.RAF", goster:false}
+     let raf12={kod: "1.2.RAF", goster:false}
+     let raf13={kod: "1.3.RAF", goster:false}
+     let raf14={kod: "1.4.RAF", goster:false}
+     let raf15={kod: "1.5.RAF", goster:false}
+
+     let raf21={kod: "2.1.RAF", goster:false}
+     let raf22={kod: "2.2.RAF", goster:false}
+     let raf23={kod: "2.3.RAF", goster:false}
+     let raf24={kod: "2.4.RAF", goster:false}
+     let raf25={kod: "2.5.RAF", goster:false}
+
+     let raf31={kod: "3.1.RAF", goster:false}
+     let raf32={kod: "3.2.RAF", goster:false}
+     let raf33={kod: "3.3.RAF", goster:false}
+     let raf34={kod: "3.4.RAF", goster:false}
+     let raf35={kod: "3.5.RAF", goster:false}
+
+     let raf41={kod: "4.1.RAF", goster:false}
+     let raf42={kod: "4.2.RAF", goster:false}
+     let raf43={kod: "4.3.RAF", goster:false}
+     let raf44={kod: "4.4.RAF", goster:false}
+     let raf45={kod: "4.5.RAF", goster:false}
+
+     let raf51={kod: "5.1.RAF", goster:false}
+     let raf52={kod: "5.2.RAF", goster:false}
+     let raf53={kod: "5.3.RAF", goster:false}
+     let raf54={kod: "5.4.RAF", goster:false}
+     let raf55={kod: "5.5.RAF", goster:false}
+
+     let raflar= [
+     [raf51,raf52,raf53,raf54,raf55]
+     [raf41,raf42,raf43,raf44,raf45]
+     [raf31,raf32,raf33,raf34,raf35]
+     [raf21,raf22,raf23,raf24,raf25]
+     [raf11,raf12,raf13,raf14,raf15]
+
+     ];
+
+     function rafolustur(){
+        console.clear();
+        let satir="";
+        for (let i=0; i<raflar.length;i++){
+            for (let j=0; j<5;j++){
+                satir+="|"+(raflar[i][j].goster ? raflar[i][j].kod: "-----")+"";
+                
+            }
+            console.log(satir);
+            console.log("---------------");
+            satir="";
+        }
+     }
+
+     rafolustur();
+
+     Rafları Oluşturduk Şimdi Kullanıcı Ekrandan Kitap Girince Kitabı Bulduralım.
+
+     function kodBul(kitapIsmi){
+        let rafKod= null;
+        kitaplar.forEach(function(kitap){
+            if(kitap.isim.toUpperCase().includes(kitapIsmi.toUpperCase(),0))
+            {rafKod=kitap.raf;
+        }
+        });
+        return rafKod;
+    }
+
+    function raftaGoster(rafKodu){
+        for(let i=0;i<raflar.length;i++){
+            for (let j=0, j<5; j++){
+                if (raflar[i][j].kod==rafKodu){
+                    raflar[i][j].goster=true;
+                    break;
+                }
+            }
+        }
+    }
+
+    rafOlustur();
+    let kitapIsim= prompt("Lütfen Bir Kitap İsmi Giriniz!");
+    let rafKod= bul(kitapIsmi);
+    console.log(rafKod);
+
+  
+
+   if (rafKod!=null){
+    raftaGoster(rafKod);
+
+    }else{
+      alert("Girdiğiniz kitap kütüphanemizde bulunmamaktadır.")
+    }
+
 */
